10336244338150247d9764ece5fb67c1c14231d7
shaoxi2010 <shaoxi2010@qq.com>
2023-03-26 18:10:49 +0800

    改进程序，并兼容edge浏览器

--- main.py
+++ main.py
@@ -1,62 +1,183 @@
-from web import Wenjuan
+from typing import List, Optional, Tuple
+from web import Wenjuan,Question,Submit
+import random
 from flet import (
     app,
     Row,
+    Column,
+    ListView,
     Ref,
     ControlEvent,
     TextField,
     Text,
     IconButton,
     ElevatedButton,
+    Radio,
+    RadioGroup,
     SnackBar,
+    UserControl,
     Page
 )
 
+
+def clamp_number(num, a, b):
+    return max(min(num, max(a,b)), min(a,b))
+
+class FletQuestion(UserControl):
+    def __init__(self, question, answers):
+        super().__init__()
+        self.question = question
+        self.answers = answers
+        self.choose = 0
+
+    def build(self):
+        return Column([
+            Text(self.question),
+            RadioGroup(
+                Row(
+                    [Radio(label=val, value=i) for (i, val) in enumerate(self.answers)]
+                ),
+                on_change=self.__radiogroup_onchange,
+                value=0
+            ),
+
+        ])
+
+    def __radiogroup_onchange(self, e: ControlEvent):
+        self.choose = int(e.control.value)
+        self.update()
+
+    def choose_index(self) -> int:
+        return self.choose
+
+
+class FletQuestions(UserControl):
+    def __init__(self, ref: Optional[Ref] = None):
+        super().__init__(ref=ref)
+        self.questions = ListView(auto_scroll=True,height=400, expand=1)
+    def build(self):
+        return self.questions
+
+    def update_url(self, url):
+        self.questions.controls.clear()
+        (_questions, _) = self.page.webwjx.fetch_questions(url)
+        for x in _questions:
+            self.questions.controls.append(FletQuestion(x.get_question(), x.get_options()))
+        self.update()
+
+    def choose(self, index:int) -> int:
+        return self.questions.controls[index].choose_index()
+
+class FletCouter(UserControl):
+    def __init__(self, name, value, ref: Optional[Ref] = None, range:Optional[Tuple[int, int]] = None):
+        super().__init__(ref= ref)
+        self.__value = str(value)
+        self.__lable = name
+        self.__text = Ref[Text]()
+        self.__range = range
+    def build(self):
+        return Row(
+                    [
+                        Text(self.__lable),
+                        IconButton('remove', on_click=self.minus_click),
+                        Text(ref=self.__text, value=self.__value,text_align='right'),
+                        IconButton('add', on_click=self.plus_click),
+                    ],
+                )
+
+    def minus_click(self,e: ControlEvent):
+        val = int(self.__text.current.value) - 1
+        if self.__range is None:
+            self.__text.current.value = str(val)
+        else:
+            (min, max) = self.__range
+            self.__text.current.value = str(clamp_number(val, min, max))
+        self.update()
+
+    def plus_click(self, e: ControlEvent):
+        val = int(self.__text.current.value) + 1
+        if self.__range is None:
+            self.__text.current.value = str(val)
+        else:
+            (min, max) = self.__range
+            self.__text.current.value = str(clamp_number(val, min, max))
+        self.update()
+
+    def value(self):
+        return int(self.__text.current.value)
+
 url = Ref[TextField]()
-count = Ref[Text]()
+submitcount = Ref[FletCouter]()
+randomrange = Ref[FletCouter]()
+precision = Ref[FletCouter]()
+questions = Ref[FletQuestions]()
 
 def submit_click(e: ControlEvent):
-    ct = int(count.current.value)
-    if ct < 0:
-        ct = 0
-    e.page.show_snack_bar(SnackBar(Text('有点慢，等等浏览器响应'), open=True))
+    _ct = submitcount.current.value()
+    _rate = precision.current.value()
+    _range = randomrange.current.value()
+    e.page.show_snack_bar(SnackBar(Text('开始自动填写问卷'), open= True))
     e.page.update()
-    wjx = Wenjuan()
-    for _ in range(ct):
-        wjx.answer_question(url.current.value)
+    for num in range(_ct):
+        (_questions, _submit) = e.page.webwjx.fetch_questions(url.current.value)
+        for (index, question) in enumerate(_questions):
+            if question.get_options():
+                i = questions.current.choose(index)
+                if random.random() * 100 > _rate:
+                    i = clamp_number(i + random.randint(-_range, _range), 0, len(question.get_options())-1)
+                question.click(i)
+        _submit.click()
+        e.page.show_snack_bar(SnackBar(Text(f'已完成问卷{num + 1}次'), open= True))
+        e.page.update()
     e.page.show_snack_bar(SnackBar(Text('完成啦'), open= True))
     e.page.update()
 
-def minus_click(e: ControlEvent):
-    count.current.value = str(int(count.current.value) - 1)
+
+
+def url_submit(e: ControlEvent):
+    e.page.show_snack_bar(SnackBar(Text('加载问卷星表单'), open= True))
     e.page.update()
-
-
-def plus_click(e: ControlEvent):
-    count.current.value = str(int(count.current.value) + 1)
-    e.page.update()
-
-
+    url = e.control.value
+    questions.current.update_url(url)
 
 def main(page: Page):
-    page.title = '问卷星自动填写器'
+    def close(e: ControlEvent):
+        page.show_snack_bar(SnackBar(Text('正在关闭浏览器和程序，请稍等'), open= True))
+        page.update()
+        del page.webwjx
+        page.window_destroy()
+    page.title = '问卷星自动填写器 by shaoxi2010 (window edge)'
+    page.window_height = 600
+    page.window_width = 800
     page.window_resizable = False
-    page.window_height = 200
+    page.webwjx = Wenjuan()
+    page.window_prevent_close = True
     page.add(
-        TextField(
-            ref = url,
-            hint_text='输入调查问卷表格地址',
-            autofocus= True,
+        Row(
+            [
+                TextField(
+                    ref = url,
+                    hint_text='输入调查问卷表格地址',
+                    autofocus= True,
+                    on_submit=url_submit,
+                    width=700,
+                ),
+                IconButton('exit_to_app',on_click=close)
+            ],
+            alignment="right",
+            height=50
         ),
         Row(
             [
-                Text('总提交次数：'),
-                IconButton('remove', on_click=minus_click),
-                Text(ref=count, value=0,text_align='right', width=100),
-                IconButton('add', on_click=plus_click),
+                FletCouter('准确率', 100, ref=precision, range=(0,100)),
+                FletCouter('误差范围', 0, ref=randomrange, range=(0,100)),
+                FletCouter('提交次数', 0, ref=submitcount, range=(0,100)),
                 ElevatedButton('开始填表', width=200, on_click=submit_click)
             ],
-            alignment='center',
-        )
+            height=50,
+        ),
+        FletQuestions(ref=questions)
     )
-app(target=main)
+
+if __name__ == "__main__":
+    app(target=main)
--- web.py
+++ web.py
@@ -2,20 +2,83 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.remote.webdriver import WebDriver
 
+from typing import Optional, List,Tuple
 import time
 import random
-import threading
-import pdb
+import platform
+import sys
+import os
+
+#生成资源文件目录访问路径
+def resource_path(relative_path):
+    if getattr(sys, 'frozen', False): #是否Bundle Resource
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+class Question:
+    def __init__(self, question:WebElement, options: Optional[list[WebElement]] = None) -> None:
+        self.__question = question
+        self.__options = options
+
+    def click(self, index:int):
+        if self.__options is not None and isinstance(self.__options, List):
+            self.__options[index].click()
+        else:
+            print("注意接口查找错误")
+
+    #//a[@class="rate-off rate-offlarge"]
+    def get_options(self) -> List[str]:
+        if self.__options is None:
+            return []
+        return [x.get_attribute('title') for x in self.__options]
+
+    #//div[@class="field-label"]
+    def get_question(self) -> str:
+        return self.__question.text
+
+class Submit:
+    def __init__(self, driver:WebDriver) -> None:
+        self.__diver = driver
+
+    def click(self):
+        if isinstance(self.__diver, WebDriver):
+            self.__diver.find_element(By.ID, 'ctlNext').click()
+            time.sleep(0.5)
+            try:
+                comfirmdel = self.__diver.find_element(
+                    By.XPATH, '//a[@class="layui-layer-btn0"]')
+                comfirmdel.click()
+                time.sleep(0.5)
+                validation = self.__diver.find_element(By.XPATH, '//div[@id="rectMask"]')
+                validation.click()
+                time.sleep(4)
+            except:
+                pass
+        else:
+            raise TypeError
+
 
 class Wenjuan:
     def __init__(self) -> None:
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        # chrome 88 或更高版本的反爬虫特征处理
-        options.add_argument('--disable-blink-features=AutomationControlled')
-        self.driver = webdriver.Chrome()
-        with open('stealth.min.js') as stealth:
+        if platform.system() == 'Windows':
+            options = webdriver.EdgeOptions()
+            options.add_experimental_option('excludeSwitches', ['enable-automation'])
+            options.add_argument('--disable-blink-features=AutomationControlled')
+            options.add_argument('--headless')
+            self.driver = webdriver.Edge(options=options)
+        else:
+            options = webdriver.ChromeOptions()
+            options.add_experimental_option('excludeSwitches', ['enable-automation'])
+            # chrome 88 或更高版本的反爬虫特征处理
+            options.add_argument('--disable-blink-features=AutomationControlled')
+            options.add_argument('--headless')
+            self.driver = webdriver.Chrome(options=options)
+        with open(resource_path('stealth.min.js')) as stealth:
             stealthjs = stealth.read()
             self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
                 "source": stealthjs
@@ -24,51 +87,37 @@ class Wenjuan:
     def __del__(self):
         self.driver.quit()
 
-    def answer_question(self, url: str):
+    def reload(self, url: str):
         self.driver.get(url)
         self.driver.execute_script(
             "var q=document.documentElement.scrollTop=0")
-        time.sleep(2)
-        WebDriverWait(self.driver, 1000).until(
-            EC.presence_of_element_located((By.ID, 'ctlNext'))
-        )
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'ctlNext')))
 
-        total = 100
-        # 1、获取题目，遍历
-        titles = self.driver.find_elements(
-            By.XPATH, '//div[@class="field-label"]')
-        for (i, title) in enumerate(titles):
+    def fetch_questions(self, url: str) -> Tuple[List[Question], Submit]:
+        elements = []
+        self.reload(url)
+        questions = self.driver.find_elements(By.XPATH, '//div[@class="field-label"]')
+        for (i, title) in enumerate(questions):
             # 获取每个问题的
-            print(title.text)
-            answers = self.driver.find_elements(
-                By.XPATH, f'//div[@id="div{i+1}"]//a[@class="rate-off rate-offlarge"]')
-            print([x.get_attribute('title') for x in answers])
-            if len(answers) == 0:
-                continue
-           
-            check = 0 if random.random() < 0.8 else 1
-            total -= check
-            if total <= 98:
-                check = 0
-            answers[check].click()
-            time.sleep(0.2)
-        # 点击提交
-        self.driver.find_element(By.ID, 'ctlNext').click()
-        time.sleep(0.5)
+            answers = self.driver.find_elements(By.XPATH, f'//div[@id="div{i+1}"]//a[@class="rate-off rate-offlarge"]')
+            elements.append(Question(title, answers if len(answers) != 0 else None))
+        return (elements, Submit(self.driver))
 
-        try:
-            comfirmdel = self.driver.find_element(
-                By.XPATH, '//a[@class="layui-layer-btn0"]')
-            comfirmdel.click()
-            time.sleep(0.5)
-            validation = self.driver.find_element(By.XPATH, '//div[@id="rectMask"]')
-            validation.click()
-        except:
-            pass
-        
-        time.sleep(4)
-        print(f'已填写完问卷')
 
 if __name__ == '__main__':
     wenjuan = Wenjuan()
-    wenjuan.answer_question('https://www.wjx.cn/vm/tibXV8t.aspx')
+    # wenjuan.answer_question('https://www.wjx.cn/vm/tibXV8t.aspx')
+    wenjuan.fetch_questions('https://www.wjx.cn/vm/tibXV8t.aspx')
+    (questions, submit) = wenjuan.fetch_questions('https://www.wjx.cn/vm/tibXV8t.aspx')
+
+    for question in questions:
+        print(question.get_question())
+        print(question.get_options())
+        options = len(question.get_options())
+        if options > 0 :
+            index = random.randint(0, len(question.get_options())-1)
+            print(index)
+            question.click(index)
+
+    submit.click()
+    time.sleep(4)
